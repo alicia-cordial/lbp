df900c6acdcedd38293d6b7b51de4d09899e91bb
Cordial <alicia.cordial@laplateforme.io>
2021-05-11 13:45:41 +0200

    suite recherche autocompletion

--- views/home.php
+++ views/home.php
@@ -1,15 +1,6 @@
-<<<<<<< Updated upstream
 
-
-=======
->>>>>>> Stashed changes
 <main class="main_index">
       <form id="form_autocomplete" class="form_autocomplete" action="recherche.php" method="get">
-        <input type="text" name="search" class="input_index" id="philosophes" placeholder="Qui recherchez-vous ?">
+        <input type="text" name="search" class="input_index" id="article" placeholder="Que recherchez-vous ?">
       </form>
-<<<<<<< Updated upstream
-    </main>
-=======
-    </main>
-  
->>>>>>> Stashed changes
+</main>
