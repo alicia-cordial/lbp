a6e5803e0404cf231f325aa62c1403d5abf40daf
Cordial <alicia.cordial@laplateforme.io>
2021-05-11 15:24:07 +0200

    continuation essai barre recherche

--- js/shop.js
+++ js/shop.js
@@ -1,36 +1,17 @@
-$(function() {
+window.addEventListener("DOMContentLoaded", (function() {
 
-    $("#article").autocomplete({
+    window.addEventListener("#article").autocomplete({
         source: "../view/autocompletion.php"
     });
 
 
-});
-
+}));
+/*
 var url = window.location.href;
 console.log(url);
 
-if (url === 'http://localhost/lbp/index.php' || url === 'http://localhost/lbp/') {
-    $('.bar_header').hide();
+if (url === 'http://localhost/lbp/home.php' || url === 'http://localhost/lbp/') {
+    window.addEventListener('.bar_header').hide();
 } else {
-    $('.bar_header').show();
-}
-
-
-// (C) ATTACH AUTOCOMPLETE TO INPUT FIELDS
-window.addEventListener("DOMContentLoaded", function() {
-    ac.attach({
-        target: "demoA",
-        data: "../views/Database.php",
-        post: { type: "name" }
-    });
-
-    ac.attach({
-        target: "demoB",
-        data: "2b-search.php",
-        post: { type: "email" },
-        // OPTIONAL
-        delay: 1000,
-        min: 3
-    });
-});
+    window.addEventListener('.bar_header').show();
+}*/
--- models/autocompletion.php
+++ models/autocompletion.php
@@ -3,6 +3,17 @@
 
 class Autocompletion extends Database{
 
+
+function getInfos(){
+    $id = $_GET['id'];
+    $req = $this->pdo->prepare(" SELECT * FROM article WHERE id = '$id' ");
+    $req->execute();
+    $article = $req->fetch();
+}
+
+
+
+
     function chargeBDD(){
 
 // Requête comparant le term avec les données de la bdd
@@ -11,7 +22,7 @@ $term = $_GET["term"];
 try
 {
 
-  $query = $this->pdo->query("SELECT nom FROM article WHERE titre LIKE '%$term%' LIMIT 8");
+  $query = $this->pdo->query("SELECT titre FROM article WHERE titre LIKE '%$term%' LIMIT 8");
   $tableau = $query->fetchAll(PDO::FETCH_COLUMN, 0);
 
   echo json_encode($tableau);
@@ -32,10 +43,10 @@ if(isset($_GET['search'])){
 
   $search = $_GET['search'];
 
-  $query = $this->pdo->query("SELECT * FROM philosophes WHERE nom LIKE '%$search%'");
+  $query = $this->pdo->query("SELECT * FROM article WHERE titre LIKE '%$search%'");
 
 }else{
-    header('Location:index.php');
+    header('Location:home.php');
   }
 
 }
