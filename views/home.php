c95a2a9415d29016ea30ba674c9464f7f3665b49
huongmay-nguyenphuoc <huong-may.nguyen-phuoc@laplateforme.io>
2021-05-14 15:31:56 +0200

    main - suppression autocomplétion

--- views/home.php
+++ views/home.php
@@ -1,6 +1,3 @@
+<main>
 
-<main class="main_index">
-      <form id="form_autocomplete" class="form_autocomplete" action="views/autocompletion/recherche.php" method="get">
-        <input type="text" name="search" class="input_index" id="article" placeholder="Que recherchez-vous ?">
-      </form>
 </main>
