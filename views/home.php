0d29d91dca31e1420cc0f45b08bb10cb3494d37b
Cordial <alicia.cordial@laplateforme.io>
2021-05-25 15:50:11 +0200

    deuxième barre de recherche

--- views/home.php
+++ views/home.php
@@ -1,4 +1,30 @@
 <main>
 
-HOME
+<div class="form_rechercheavancee">
+
+<form id="form_autocompleteavancee" class="form_autocompleteavancee"  method="get">
+    <label for="cat">Categories</label>
+    <select id="cat" name="cat"> 
+        <option>Voiture</option>
+        <option>Bijoux</option>
+        <option>Vêtements</option>
+    </select>
+    <input type="text" name="search_all" class="input_index" id="search_all" placeholder="Que recherchez-vous ?">
+    <input type="text" name="search_zip" class="search_zip" id="search_zip" placeholder="Quelle région?">    
+</form>
+
+
+
+<form id="form_usersearch" class="form_usersearch"  method="get">
+    <input type="text" name="search_user" class="input_user" id="search_user" placeholder="Qui recherchez-vous ?">
+    <input type="text" name="search_zip" class="search_zip" id="search_zip" placeholder="Quelle région?">    
+</form>
+
+
+</div>
+
+<div>
+    <div id="result_search"></div>
+</div>
+
 </main>
