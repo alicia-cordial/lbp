8b10dcf7aff269456c3c52bd9ed182b3806d0359
huongmay-nguyenphuoc <huong-may.nguyen-phuoc@laplateforme.io>
2021-06-07 15:18:40 +0200

    modal messagerie test

--- test.php
+++ test.php
@@ -1,59 +1,45 @@
-<main>
+HTML
 
-<div class="form_rechercheavancee">
-
-
-    <section id="objet">
-    
-    <span id="form_vendeur">Vendeur</span>  
-
-        <form id="form_autocompleteavancee" class="form_index"  method="post" name="formSaisie">
-            <label for="nom">Categories</label>
-            <select id="nom" name="nom"> 
-                <option>Voiture</option>
-                <option>Bijoux</option>
-                <option>Vêtements</option>
-            </select>
-            <input type="text" name="titre" class="titre" id="titre" placeholder="Que recherchez-vous ?">
-            <input type="text" name="zip" class="zip" id="zip" placeholder="Quelle région?">    
-        <button type="submit" value="Submit">Submit</button>
-        </form>
-
-    </section>
-
-    <section id="vendeur">
-    
-    <span id="form_objet">Objet</span>
-        
-        <form id="form_usersearch" class="form_index"  method="post">
-            <input type="text" name="user" class="input_user" id="search_user" placeholder="Qui recherchez-vous ?">
-            <input type="text" name="zip" class="search_zip" id="search_zip" placeholder="Quelle région?">    
-            <button type="submit" value="submit">Submit</button>
-        </form>
-
-    </section>
+//MODAL
+<div id="ex1" class="modal">
+    <div id="nameDestinataire"></div>
+    <form id='newMessage'>
+        <input placeholder='votre message' required>
+        <button type='submit'>Envoyer</button>
+    </form>
+    <div id="infoMessage"></div>
+    <a href="#" rel="modal:close">Close</a>
 </div>
 
-<div>
-    <div id="result_search"></div>
-</div>
-
-</main>
 
 
-<script type="text/javascript">
 
-function valider() {
-  // si la valeur du champ prenom est non vide
-  if(document.formSaisie.titre.value != "") {
-    // alors on envoie le formulaire
-    document.formSaisie.Submit();
-  }
-  else {
-    // sinon on affiche un message
-    alert("Saisissez le titre");
-  }
+//BOUTON DECLENCHEUR
+<a id ='" + user.id_vendeur + "' class='contactUser' href='#ex1' rel='modal:open'>Contacter le vendeur</a>
+
+
+//JS
+
+$('body').on('click', '.contactUser', function (event) {
+$('#nameDestinataire').empty()
+let idDestinataire = //id
+let loginDestinataire = //login
+$('#nameDestinataire').append('<p>'+loginDestinataire+'</p>')
+$('body').on('submit', '#newMessage', function (event) {
+
+console.log($('#newMessage input').val())
+event.preventDefault()
+$.post(
+'API/apiMessagerie', {
+action: 'sendNewMessage',
+idDestinataire: idDestinataire,
+messageContent: $('#newMessage input').val()
+},
+function (data) {
+let message = JSON.parse(data);
+console.log(data);
+$('#infoMessage').append("<p>Message envoyé !</p>")
 }
-
-//]]>
-</script>
+)
+})
+});
